0727c95bee26fba6df6b259d098a1866d8014f24
Etienne Herzig <125372778+EtienneHerzig@users.noreply.github.com>
2023-03-28 18:36:41 +0200

    impl Display for ProbabilityAmplitude

--- src/amplitude.rs
+++ src/amplitude.rs
@@ -1,3 +1,4 @@
+use std::fmt::Display;
 use fraction::DynaDecimal as Fraction;
 use fraction::{
     GenericDecimal,
@@ -20,5 +21,11 @@ impl ProbabilityAmplitude {
 impl Default for ProbabilityAmplitude {
     fn default() -> Self {
         return Self { real: 0.into(), imag: 0.into() }
+    }
+}
+
+impl Display for ProbabilityAmplitude {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
     }
 }
