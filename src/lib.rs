199821eec67c44358d6bcf8bef91af5f6bceba3e
Etienne Herzig <125372778+EtienneHerzig@users.noreply.github.com>
2023-03-28 18:39:24 +0200

    Added default_fmt Test for ProbabilityAmplitude

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,13 @@ mod amplitude;
 
 #[cfg(test)]
 mod tests {
-    
+    use super::amplitude::ProbabilityAmplitude;
+
+    #[test]
+    fn default_fmt() {
+        let expected = "0 + 0i".to_owned();
+        let result = format!("{}", ProbabilityAmplitude::default());
+
+        assert_eq!(expected, result);
+    }
 }
