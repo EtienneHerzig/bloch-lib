5848c31f3b7b196ec321d12ac6f8eec972c9ef71
Etienne Herzig <125372778+EtienneHerzig@users.noreply.github.com>
2023-03-28 19:06:49 +0200

    impl new for Qubit

--- src/qubit.rs
+++ src/qubit.rs
@@ -5,3 +5,9 @@ pub struct Qubit {
     alpha: ProbabilityAmplitude,
     beta: ProbabilityAmplitude
 }
+
+impl Qubit {
+    pub fn new(alpha: ProbabilityAmplitude, beta: ProbabilityAmplitude) -> Self {
+        return Self { alpha: alpha, beta: beta };
+    }
+}
